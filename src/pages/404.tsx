b45d29f50b622792840c7a2adba0c100a151a38f
Letfordlay <laytonletford2001@gmail.com>
2024-05-15 03:44:10 -0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
 export default function Custom404() {
-    return <p>Error 404 Page not found</p>
+    return (
+		<>
+			<p>
+				Error 404 Page not found
+			</p>
 	
-	<div>
-		<p>
-			<img src="/img/glitchedlogo.gif" alt="Glitched Logo" />
-		</p>
-	</div>;
+			<p>
+				<img src="/img/glitchedlogo.gif" alt="Glitched Logo" />
+			</p>
+		</>
+	);
 }
