0f87b8c4ffda7da9961dc4d91a6b943e3231e12d
Letfordlay <laytonletford2001@gmail.com>
2025-04-04 06:01:52 -0500

    testing embed instead of iframe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,7 @@ export default function Home() {
 			</p>
 
             <p>
-                This website was made with the idea of looking old while using current coding techniques.
-				
-				You'll find many old and new games created by an assortment of talented individuals.
+                Welcome to my website!
             </p>
         </>
     );
