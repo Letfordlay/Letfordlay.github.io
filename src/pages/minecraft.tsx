f72e8143d60961b59e6de199cee430d88231bc13
Letfordlay <laytonletford2001@gmail.com>
2025-04-04 15:23:38 -0500

    Update minecraft.tsx

--- src/pages/minecraft.tsx
+++ src/pages/minecraft.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { mcversions } from "@/games";
 
-export default function Home() {
+export default function Minecraft() {
     return (
             <center className="list">
                 <p className="list-title">Versions</p>
