d8c51f8a106e4da9a6b684baa6f11db1d0193e39
Letfordlay <laytonletford2001@gmail.com>
2025-04-06 06:18:26 -0500

    cleaning up extra files for Marble Blast Ultra

--- public/Games/Marble Blast Ultra/src/collision/gjk/GJKShape.hx
+++
@@ -1,8 +0,0 @@
-package collision.gjk;
-
-import h3d.Vector;
-
-interface GJKShape {
-	function getCenter():Vector;
-	function support(dir:Vector):Vector;
-}
--- public/Games/Marble Blast Ultra/src/octree/IOctreeElement.hx
+++
@@ -1,6 +0,0 @@
-package octree;
-
-interface IOctreeElement {
-	function getElementType():Int;
-	function setPriority(priority:Int):Void;
-}
