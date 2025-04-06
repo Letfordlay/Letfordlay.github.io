df5571325fd8003560c86d277131cc1e4f446cd5
Letfordlay <laytonletford2001@gmail.com>
2025-04-06 09:13:10 -0500

    Update [slug].tsx

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -25,7 +25,7 @@ export default function Game({ slug }: InferGetStaticPropsType<typeof getStaticP
     const game = games[slug] || flashGames[slug] || mcversions[slug];
 
     return (
-        <div className=content>
+        <div className="content">
             <Head>
                 <title>{game.name} - Letfordlay Gaming</title>
             </Head>
