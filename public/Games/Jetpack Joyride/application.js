2c239e93d8ab1b3f8dc26a4f9340d2aab44692df
Letfordlay <laytonletford2001@gmail.com>
2025-04-08 10:21:51 -0500

    Update application.js

--- public/Games/Jetpack Joyride/application.js
+++ public/Games/Jetpack Joyride/application.js
@@ -93,7 +93,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.json';
+      var settings = '/Games/Jetpack Joyride/src/settings.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
