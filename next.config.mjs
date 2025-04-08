27e1ef4cc517db9eacb69760831af7a6ca7627fc
Letfordlay <laytonletford2001@gmail.com>
2025-04-08 12:52:09 -0500

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     reactStrictMode: true,
     output: "export",
     trailingSlash: true
+	rewrites: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+},
 };
 
 export default nextConfig;
